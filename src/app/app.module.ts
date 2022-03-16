d3d82f6654c41ad61ded1aa4964a21929794703b
Karthik Sreenivasan <karthik.sreenivasan.work@gmail.com>
2022-03-16 12:01:53 +0530

    Installed angular material to this project ng add @angular/material
    Angular Material Link: https://material.angular.io/guide/getting-started
    
    The ng add command will additionally perform the following actions:
    
    Add project dependencies to package.json
    Add the Roboto font to your index.html
    Add the Material Design icon font to your index.html
    Add a few global CSS styles to:
    Remove margins from body
    Set height: 100% on html and body
    Set Roboto as the default application font

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { ProducttrackingComponent } from './producttracking/producttracking.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { ProducttrackingComponent } from './producttracking/producttracking.comp
     ProducttrackingComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
